f2ad50cbdd914f0c2489b0d499409084217a3a79
Brandon Lyons <brandon_lyons98@yahoo.com>
2023-10-26 12:53:49 -0400

    Add footer component file

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      Copyright © 2022 Brandon-Lyons98
+    </footer>
+  )
+}
+
+export default Footer;
